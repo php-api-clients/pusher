4c51b469d104c933b8b7ae50ed58750348f833bf
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-05-22 00:53:56 +0200

    Not working Rx\WebSocket client

--- src/AsyncClient.php
+++ src/AsyncClient.php
@@ -3,29 +3,32 @@ declare(strict_types=1);
 
 namespace WyriHaximus\Pusher;
 
-use Ratchet\Client\Connector;
-use Ratchet\Client\WebSocket;
-use Ratchet\RFC6455\Messaging\MessageInterface;
 use React\EventLoop\LoopInterface;
 use Rx\Observable;
 use Rx\ObservableInterface;
+use Rx\Observer\CallbackObserver;
 use Rx\ObserverInterface;
 use Rx\React\Promise;
+use Rx\Scheduler\EventLoopScheduler;
+use Rx\Websocket\Client;
 use WyriHaximus\ApiClient\Transport\Client as Transport;
 use WyriHaximus\ApiClient\Transport\Factory;
 use function React\Promise\resolve;
+use function EventLoop\getLoop;
+use function EventLoop\setLoop;
 
 class AsyncClient
 {
     protected $transport;
     protected $app;
     protected $url;
-    protected $socket;
+    protected $client;
     protected $connection;
     protected $channels = [];
 
     public function __construct(LoopInterface $loop, string $app, Transport $transport = null)
     {
+        setLoop($loop);
         /*if (!($transport instanceof Transport)) {
             $transport = Factory::create($loop, [
                     'resource_namespace' => 'Async',
@@ -35,26 +38,24 @@ class AsyncClient
 
         $this->app = $app;
 
-        $this->url = 'wss://ws.pusherapp.com:443/app/' .
+        $this->url = 'wss://ws.pusherapp.com/app/' .
             $this->app .
             '?client=wyrihaximus-php-pusher-client&version=0.0.1&protocol=7'
         ;
-        $connector = new Connector($loop);
-        $this->socket = $connector($this->url);
-        $this->connection = Promise::toObservable($this->socket)
-            ->flatMap(function (WebSocket $socket) {
-                $this->socket = resolve($socket);
-                return Observable::create(function (ObserverInterface $observer) use ($socket) {
-                    $socket->on('message', function (MessageInterface $message) use ($observer) {
-                        $observer->onNext($message);
-                    });
-                    $socket->on('close', function ($code, $reason) {
 
-                    });
-                });
-            })->map(function (MessageInterface $message) {
-                return json_decode((string)$message, true);
-            });
+        $this->client = new Client($this->url);
+        $this->connection = Observable::create(function (ObserverInterface $observer) {
+            $this->client->subscribe(new CallbackObserver(function ($ms) use ($observer) {
+                $ms->subscribe(new CallbackObserver(
+                    function ($message) use ($observer) {
+                        $observer->onNext($message);
+                    }
+                ), new EventLoopScheduler(getLoop()));
+            }));
+        }, new EventLoopScheduler($loop))->map(function ($message) {
+            var_export($message);
+            return json_decode((string)$message, true);
+        });
     }
 
     public function subscribe(string $channel): ObservableInterface
@@ -76,8 +77,6 @@ class AsyncClient
 
     public function send(array $message)
     {
-        $this->socket->then(function (WebSocket $socket) use ($message) {
-            $socket->send(json_encode($message));
-        });
+        $this->client->send(json_encode($message));
     }
 }
