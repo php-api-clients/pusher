4cb87d7f4c671b75ef29f52cdd3c42225d692d06
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-05-21 23:28:24 +0200

    No need to assign the function to a $variable, just pass it into the observer right away

--- examples/reddit.php
+++ examples/reddit.php
@@ -7,12 +7,6 @@ use WyriHaximus\Pusher\AsyncClient;
 
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
 
-$display = function ($event) {
-    echo 'Channel: ', $event['channel'], PHP_EOL;
-    echo 'Event: ', $event['event'], PHP_EOL;
-    echo 'Data: ', $event['data'], PHP_EOL;
-};
-
 $loop = Factory::create();
 
 /**
@@ -24,7 +18,11 @@ $client = new AsyncClient($loop, '50ed18dd967b455393ed');
 $subReddits = $argv;
 array_shift($subReddits);
 foreach ($subReddits as $subReddit) {
-    $client->subscribe($subReddit)->subscribe(new CallbackObserver($display));
+    $client->subscribe($subReddit)->subscribe(new CallbackObserver(function($event) {
+        echo 'Channel: ', $event['channel'], PHP_EOL;
+        echo 'Event: ', $event['event'], PHP_EOL;
+        echo 'Data: ', $event['data'], PHP_EOL;
+    }));
 }
 
 $loop->run();
