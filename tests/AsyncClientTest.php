0ee9b2ebd512fb0778b1fec767a68cc0bd08c998
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-05-21 21:40:18 +0200

    test place holder

---
+++ tests/AsyncClientTest.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace WyriHaximus\Tests\Pusher;
+
+class AsyncClientTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
