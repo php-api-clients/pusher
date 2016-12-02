b6e664c4eabffb1a13527d7caf0c603da6cea62e
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-12-02 07:58:20 +0100

    Also added App ID note to reddit key file

--- examples/reddit.key.php
+++ examples/reddit.key.php
@@ -1,2 +1,7 @@
 <?php declare(strict_types=1);
+
+/**
+ * The App ID isn't a secret and comes from a Pusher blog post:
+ * @link https://blog.pusher.com/pusher-realtime-reddit-api/
+ */
 return '50ed18dd967b455393ed';
