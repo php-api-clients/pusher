de085a6f89b0eecdd6d1189efa33497c4b15f749
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-03-31 21:42:07 +0200

    Removed tests bootstrap

--- tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use React\EventLoop\Factory;
-use function EventLoop\setLoop;
-
-require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
-
-setLoop(Factory::create());
