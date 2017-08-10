57b6325428d66263afe856fd1dd83efc13b6abfa
David Dan <davidwdan@gmail.com>
2017-08-10 18:37:51 -0400

    Removed unnecessary promises.  Observables are only converted to promises at the end of the stream.

--- examples/reddit-single.php
+++ examples/reddit-single.php
@@ -9,7 +9,7 @@ require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
  * The App ID isn't a secret and comes from a Pusher blog post:
  * @link https://blog.pusher.com/pusher-realtime-reddit-api/
  */
-$client = Client::create(require 'reddit.key.php');
+$client = new Client(require 'reddit.key.php');
 
 $client->channel((string) $argv[1], function (Event $event) {
     echo 'Channel: ', $event->getChannel(), PHP_EOL;
